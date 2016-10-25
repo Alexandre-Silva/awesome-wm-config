2dfafe595634aee39e84a601514034a502e8e346
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-10-25 15:12:27 +0100

    refac: mv panel creation in structure module

--- modules/custom/structure.lua
+++ modules/custom/structure.lua
@@ -1,5 +1,8 @@
 local awful = require("awful")
+local bashets = require("bashets")
 local beautiful = require("beautiful")
+local util = require("util")
+local wibox = require("wibox")
 
 local config = require("custom.config")
 local widgets = require("custom.widgets")
@@ -83,6 +86,121 @@ function structure.init()
 
   widgets.launcher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                              menu = main_menu })
+
+
+  -- Create a wibox for each screen and add it
+
+
+  widgets.taglist.buttons = awful.util.table.join(
+    awful.button({        }, 1, awful.tag.viewonly),
+    awful.button({ modkey }, 1, awful.client.movetotag),
+    awful.button({        }, 2, awful.tag.viewtoggle),
+    awful.button({ modkey }, 2, awful.client.toggletag),
+    awful.button({        }, 3, function (t) custom.func.tag_action_menu(t) end),
+    awful.button({ modkey }, 3, awful.tag.delete),
+    awful.button({        }, 4, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end),
+    awful.button({        }, 5, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end)
+  )
+
+  widgets.tasklist = {}
+  widgets.tasklist.buttons = awful.util.table.join(
+
+    awful.button({ }, 1, function (c)
+        if c == client.focus then
+          c.minimized = true
+        else
+          -- Without this, the following
+          -- :isvisible() makes no sense
+          c.minimized = false
+          if not c:isvisible() then
+            awful.tag.viewonly(c:tags()[1])
+          end
+          -- This will also un-minimize
+          -- the client, if needed
+          client.focus = c
+          c:raise()
+        end
+    end),
+
+    awful.button({ }, 2, function (c)
+        custom.func.clients_on_tag()
+    end),
+
+    awful.button({ modkey }, 2, function (c)
+        custom.func.all_clients()
+    end),
+
+    awful.button({ }, 3, function (c)
+        custom.func.client_action_menu(c)
+    end),
+
+    awful.button({ }, 4, function ()
+        awful.client.focus.byidx(-1)
+        if client.focus then client.focus:raise() end
+    end),
+
+    awful.button({ }, 5, function ()
+        awful.client.focus.byidx(1)
+        if client.focus then client.focus:raise() end
+  end))
+
+  -- start bashets
+  bashets.start()
+
+  for s = 1, screen.count() do
+    -- Create a promptbox for each screen
+    widgets.promptbox[s] = awful.widget.prompt()
+    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    widgets.layoutbox[s] = awful.widget.layoutbox(s)
+    widgets.layoutbox[s]:buttons(awful.util.table.join(
+                                   awful.button({ }, 1, function () awful.layout.inc(custom.config.layouts, 1) end),
+                                   awful.button({ }, 3, function () awful.layout.inc(custom.config.layouts, -1) end),
+                                   awful.button({ }, 4, function () awful.layout.inc(custom.config.layouts, -1) end),
+                                   awful.button({ }, 5, function () awful.layout.inc(custom.config.layouts, 1) end),
+                                   nil
+                                ))
+    -- Create a taglist widget
+    widgets.taglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, widgets.taglist.buttons)
+
+    -- Create a textbox showing current universal argument
+    widgets.uniarg[s] = wibox.widget.textbox()
+    -- Create a tasklist widget
+    widgets.tasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, widgets.tasklist.buttons)
+
+    -- Create the wibox
+    widgets.wibox[s] = awful.wibox({ position = "top", height = "18", screen = s })
+
+    -- Widgets that are aligned to the left
+    local left_layout = wibox.layout.fixed.horizontal()
+    left_layout:add(widgets.launcher)
+    left_layout:add(widgets.taglist[s])
+    left_layout:add(widgets.uniarg[s])
+    left_layout:add(widgets.promptbox[s])
+
+    -- Widgets that are aligned to the right
+    local right_layout = wibox.layout.fixed.horizontal()
+    if s == 1 then right_layout:add(wibox.widget.systray()) end
+    right_layout:add(widgets.new_cpuusage())
+    right_layout:add(widgets.memusage)
+    right_layout:add(widgets.bat0)
+    right_layout:add(widgets.mpdstatus)
+    --right_layout:add(widgets.audio_volume)
+    right_layout:add(widgets.volume)
+    right_layout:add(widgets.date)
+    --right_layout:add(widgets.textclock)
+    right_layout:add(widgets.layoutbox[s])
+
+    -- Now bring it all together (with the tasklist in the middle)
+    local layout = wibox.layout.align.horizontal()
+    layout:set_left(left_layout)
+    layout:set_middle(widgets.tasklist[s])
+    layout:set_right(right_layout)
+
+    widgets.wibox[s]:set_widget(layout)
+  end
+
+  util.taglist.set_taglist(widgets.taglist)
 end
 
 -- }}}
