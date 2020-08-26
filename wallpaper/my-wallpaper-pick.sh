bd778b6c9a679c9331ea7bdbf10299340afc1204
Alexandre-Silva <alexandre.s.silva@posteo.net>
2020-08-26 12:00:07 +0100

    wallpaper: now fills the image instead of scaling

--- wallpaper/my-wallpaper-pick.sh
+++ wallpaper/my-wallpaper-pick.sh
@@ -4,4 +4,4 @@ if [ -d ~/.cache/wallpapers ]; then
     cd ~/.cache/wallpapers;
 fi
 
-find . -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 feh --bg-scale
+find . -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 feh --bg-fill
